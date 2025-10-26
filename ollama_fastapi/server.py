13cce8d22c177d49533f091e519a08b51f31b2e7
Himanshudevg2000 <himanshudevgade87@gmail.com>
2025-10-26 18:27:37 +0530

    Running ollama locally with fast api

--- ollama_fastapi/server.py
+++ ollama_fastapi/server.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from ollama import Client
 
 app = FastAPI()
+
+client = Client(
+    host="http://localhost:11434/"
+)
 
 @app.get('/')
 def read_root():
     return {"data": "Fast Api setup done - its working"}
+
+
+@app.post('/chat')
+def chat(
+    message: str = Body(..., decription="The Message")
+):
+    response = client.chat(model="gemma:2b", messages=[ {"role":"user", "content":message } ])
+    
+    return {"response": response.message.content}
